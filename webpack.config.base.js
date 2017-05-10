2253e1362c9fea6af1ea62e2b2eaa99ddd887303
Herry Pattar <fake_empire@mail.ru>
2017-05-10 18:50:09 +0300

    Webpack and package.json

---
+++ webpack.config.base.js
@@ -0,0 +1,50 @@
+/**
+ * Created by Raaw on 5/9/2017.
+ */
+/**
+ * Base webpack config used across other specific configs
+ */
+
+import path from 'path';
+import webpack from 'webpack';
+import { dependencies as externals } from './app/package.json';
+
+export default {
+    externals: Object.keys(externals || {}),
+    devtool: "source-map-inline",
+    module: {
+        rules: [{
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    cacheDirectory: true
+                }
+            }
+        }]
+    },
+
+    output: {
+        path: path.join(__dirname, 'app'),
+        filename: 'bundle.js',
+        devtoolLineToLine: true,
+        // https://github.com/webpack/webpack/issues/1114
+        libraryTarget: 'commonjs2'
+    },
+
+    /**
+     * Determine the array of extensions that should be used to resolve modules.
+     */
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+        modules: [
+            path.join(__dirname, 'app'),
+            'node_modules',
+        ],
+    },
+
+    plugins: [
+        new webpack.NamedModulesPlugin(),
+    ],
+};
